8375a32067dee851f5b84fc378df394fd6fb059d
Wyatt <votranquy96@gmail.com>
2019-10-23 16:51:35 +0700

    Read Query String

--- http_header.js
+++ http_header.js
@@ -2,5 +2,6 @@ var http = require('http');
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write("Good afternoon!!!!");
+    res.write(req.url);
     res.end();
 }).listen(8080);
