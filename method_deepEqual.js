8069fbff6efb486b593f4db78378d093cb342b08
Wyatt <votranquy96@gmail.com>
2019-10-28 08:48:24 +0700

    method deepEqual()

---
+++ method_deepEqual.js
@@ -0,0 +1,7 @@
+var assert = require('assert');
+var x = { a: {n: 0} };
+var y = { a: {n: 0} };
+var z = { a: {n: 1} };
+// If 2 variable are equal -> throw an error and stop program
+assert.deepEqual(x, y);
+assert.deepEqual(x, z);
