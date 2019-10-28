073ab8376533192a57b51bc232c249b8978f0097
Wyatt <votranquy96@gmail.com>
2019-10-28 08:51:29 +0700

    method deepEqual() 2

--- method_deepEqual.js
+++ method_deepEqual.js
@@ -4,4 +4,4 @@ var y = { a: {n: 0} };
 var z = { a: {n: 1} };
 // If 2 variable are equal -> throw an error and stop program
 assert.deepEqual(x, y);
-assert.deepEqual(x, z);
+assert.deepEqual(x, z, "Throw Error");
