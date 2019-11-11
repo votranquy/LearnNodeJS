29dc60676969a1a8614669236b06a8caf5ac0e7b
Wyatt <votranquy96@gmail.com>
2019-11-11 09:04:14 +0700

    test

---
+++ test3.js
@@ -0,0 +1,40 @@
+var response = {
+    "paymentList" : [
+      {
+      "acctNo": "2",
+      "srvNo": "002",
+      "tranxId": "Writeoff Payment"
+      },
+      {
+      "acctNo": "1",
+      "srvNo": "002",
+      "tranxId": "(DO NOT USE)IPS - Payment via MIT"
+      },
+      {
+      "acctNo": "3",
+      "srvNo": "001",
+      "tranxId": "(DO NOT USE)IPS - Payment via CAD"
+      }
+  ]};
+
+
+var temp = null;
+var arr = response.paymentList;
+for( var i = 0; i < arr.length -1 ; i++){
+  for(var j = 0; j < arr.length - 1; j++){
+      if( parseInt(arr[j].srvNo) > parseInt(arr[j+1].srvNo) ){
+        temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+      if( parseInt(arr[j].srvNo) == parseInt(arr[j+1].srvNo) ){
+        if( parseInt(arr[j].acctNo) > parseInt(arr[j+1].acctNo) ){
+          temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
+        }
+      }
+    // }
+  }
+}
+console.log(arr);
