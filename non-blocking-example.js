98e4ee2946e6b2d789f615c8e3c525be58a87a25
Wyatt <votranquy96@gmail.com>
2019-11-11 09:05:10 +0700

    Blocking Example

--- non-blocking-example.js
+++ non-blocking-example.js
@@ -5,6 +5,7 @@ function readFinish1(err, data){
   if(err) console.log(err);
   console.log("Data 1:");
   console.log(data.toString());
+  return data.toString();
 }
 
 //Callback function 2
@@ -12,18 +13,22 @@ function readFinish2(err, data){
   if(err) console.log(err);
   console.log("Data 2:");
   console.log(data.toString());
+  return data.toString();
 }
 
-//Read file 1
-console.log("\n");
-console.log("Read file 1");
-fs.readFile('openfile.js', readFinish1);
-
-//Read file 2
-console.log("\n");
-console.log("Read file 2");
-fs.readFile('readfile.js', readFinish2);
-
-//end
-console.log("\n");
-console.log("Program Ended!!!\n");
+async function Call(){
+  //Read file 1
+  console.log("\n");
+  console.log("Read file 1");
+  await fs.readFile('openfile.js', readFinish1);
+  // console.log(file1);
+  //Read file 2
+  console.log("\n");
+  console.log("Read file 2");
+  await fs.readFile('readfile.js',  readFinish2);
+  // console.log(file2);
+  //end
+  console.log("\n");
+  console.log("Program Ended!!!\n");
+}
+Call();
